35453948bcf94de3907eb9df2d7dcc4a8736e512
Christian Carroll <christiancarrollofficial@gmail.com>
2026-08-21 16:52:21 -0500

    Add damage types and correct Harvestable collision channel

---
+++ Source/WoodcraftSurvival/Public/Core/Damage/DamageType_Blunt.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2026 Christian Carroll. All Rights Reserved.
+
+#pragma once
+
+#include "GameFramework/DamageType.h"
+#include "DamageType_Blunt.generated.h"
+
+/**
+ * Blunt damage type (clubs, sticks, unarmed, most handles, etc.).
+ */
+UCLASS()
+class WOODCRAFTSURVIVAL_API UDamageType_Blunt : public UDamageType
+{
+	GENERATED_BODY()
+};
